ca932e67ae1f3b7a46965c17acb1691c7ee20c98
Djabrailov <djabra26ib@lycee-ndduroc.com>
2022-03-18 14:42:10 +0100

    v0.5

--- database/db.js
+++ database/db.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 
     var pool = mysql.createPool({
         connectionLimit: 10,
-        host: 'localhost',
-        user: process.env.DB_USER,
-        host: 3306,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        host: '153.92.220.151', // 153.92.220.151
+        user: 'u463335117_ferme', // u463335117_brasserie
+        password: 'Neyufx99&', // Neyufx99&
+        database: 'u463335117_ferme', // u463335117_brasserie
+        connectTimeout: 30000
     })
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        connection.release();
+        if (err) throw err;
+    });
 
 module.exports = { pool }; // Exporte la classe pour l'utiliser dans les autres 
