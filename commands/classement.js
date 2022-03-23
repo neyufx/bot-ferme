bf9c5a0b12cabb1a28fb9cfbcdfe2e00c8b07961
Djabrailov <djabra26ib@lycee-ndduroc.com>
2022-03-23 23:58:47 +0100

    v2.0

--- commands/classement.js
+++ commands/classement.js
@@ -36,9 +36,9 @@ module.exports = {
                         const embedMessage = new MessageEmbed()
                         .setTitle(`🏆 Classement semaine du ${dateFormat(firstdate)} au ${dateFormat(lastdate)} 🏆`)
                         .addFields(
-                            {name: `─`, value: `${medals[0]} - ${capitalizeFirstLetter(result[0]['nomRp'].replace('-',' '))} : ${result[0]['totalKg']} kg`},
-                            {name: `─`, value: `${medals[1]} - ${capitalizeFirstLetter(result[1]['nomRp'].replace('-',' '))} : ${result[1]['totalKg']} kg`},
-                            {name: `─`, value: `${medals[2]} - ${capitalizeFirstLetter(result[2]['nomRp'].replace('-',' '))} : ${result[2]['totalKg']} kg`}
+                            {name: ` ─`, value: `${medals[0]} - ${capitalizeFirstLetter(result[0]['nomRp'].replace('-',' '))} : ${result[0]['totalKg']} kg`},
+                            {name: ` ─`, value: `${medals[1]} - ${capitalizeFirstLetter(result[1]['nomRp'].replace('-',' '))} : ${result[1]['totalKg']} kg`},
+                            {name: ` ─`, value: `${medals[2]} - ${capitalizeFirstLetter(result[2]['nomRp'].replace('-',' '))} : ${result[2]['totalKg']} kg`}
                         )
                         .setColor('#E67E22')
                         .setFooter('© Ferme')
