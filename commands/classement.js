557913082ecde3dcf8b33003bbdd94058f73a5a0
Djabrailov <djabra26ib@lycee-ndduroc.com>
2022-04-04 19:14:49 +0200

    v2.2

--- commands/classement.js
+++ commands/classement.js
@@ -30,7 +30,7 @@ module.exports = {
                             var yyyy = today.getFullYear();
                             return dd + '/' + mm + '/' + yyyy;
                         }
-                        let i = 1;
+                        let i = 0;
                         function capitalizeFirstLetter(string) {
                             return string[0].toUpperCase() + string.slice(1);
                         }
