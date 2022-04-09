b6594d098165dc2866eda199e505683f4d2125de
Djabrailov <djabra26ib@lycee-ndduroc.com>
2022-04-10 00:59:13 +0200

    v2.3

--- src/index.js
+++ src/index.js
@@ -79,9 +79,9 @@ bot.on('messageCreate', message => {
                 connection.query(`SELECT id FROM employees WHERE nomDossier = "${message.channel.name}"`, function(error, result,field) {  
                     if (result[0] !== undefined){
                 connection.query(`insert into dossiers(numero,quantite,nom,date,employee_id) values("${message.channel.id}","${arg1}","${message.channel.name}","${date}","${result[0]['id']}")`, function (error, results, fields) {
-                bot.commands.get('kilo').execute(message,args);
                 // When done with the connection, release it.
                 connection.release();
+                bot.commands.get('kilo').execute(message,args);
                 // Handle error after the release.
                 if (error) throw error;
                 // Don't use the connection here, it has been returned to the pool.
